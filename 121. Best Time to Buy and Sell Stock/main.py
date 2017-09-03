350307940781a96d2bbd4852762550ec0d5c0fd3
Sunny Smiler <ypciehnnu@163.com>
2017-09-03 09:18:38 +0800

    Update main.py

--- 121. Best Time to Buy and Sell Stock/main.py
+++ 121. Best Time to Buy and Sell Stock/main.py
@@ -1,1 +1,16 @@
-1
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        max_dif = 0
+        if len(prices) > 1: 
+            min_idx = 0
+            for i in range(len(prices)):
+                if prices[i] < prices[min_idx]:
+                    min_idx = i
+                if max_dif < prices[i] - prices[min_idx]:
+                    max_dif = prices[i] - prices[min_idx]
+                
+        return max_dif
