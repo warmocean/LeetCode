f128d8789097e429de66927ccff7f88428d23cac
Sunny Smiler <ypciehnnu@163.com>
2017-09-03 09:23:06 +0800

    Update main.py

--- 121. Best Time to Buy and Sell Stock/main.py
+++ 121. Best Time to Buy and Sell Stock/main.py
@@ -12,5 +12,13 @@ class Solution(object):
                     min_idx = i
                 if max_dif < prices[i] - prices[min_idx]:
                     max_dif = prices[i] - prices[min_idx]
+                    
+       '''
+        max_profit, min_price = 0, float('inf')
+        for price in prices:
+            min_price = min(min_price, price)
+            profit = price - min_price
+            max_profit = max(max_profit, profit)
+        ''' 
                 
         return max_dif
